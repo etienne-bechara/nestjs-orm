198d454952b34209b1d9775c22c931fa92e81ff6
Etienne Bechara <bechara.etienne@gmail.com>
2021-04-21 17:59:47 -0300

    fix: set suggested configs as optional

--- source/orm/orm.config.ts
+++ source/orm/orm.config.ts
@@ -5,32 +5,38 @@ import { IsIn, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validato
 @Injectable()
 export class OrmConfig {
 
+  @IsOptional()
   @InjectSecret()
   @IsIn(Object.values(AppEnvironment))
   public readonly NODE_ENV: AppEnvironment;
 
+  @IsOptional()
   @InjectSecret()
   @IsIn([ 'mongo', 'mysql', 'mariadb', 'postgresql', 'sqlite' ])
   public readonly ORM_TYPE: 'mongo' | 'mysql' | 'mariadb' | 'postgresql' | 'sqlite';
 
+  @IsOptional()
   @InjectSecret()
   @IsString() @IsNotEmpty()
   public readonly ORM_HOST: string;
 
+  @IsOptional()
   @InjectSecret()
   @Transform((o) => Number.parseInt(o.value))
   @IsNumber()
   public readonly ORM_PORT: number;
 
+  @IsOptional()
   @InjectSecret()
   @IsString() @IsNotEmpty()
   public readonly ORM_USERNAME: string;
 
-  @InjectSecret()
   @IsOptional()
+  @InjectSecret()
   @IsString() @IsNotEmpty()
   public readonly ORM_PASSWORD: string;
 
+  @IsOptional()
   @InjectSecret()
   @IsString() @IsNotEmpty()
   public readonly ORM_DATABASE: string;
